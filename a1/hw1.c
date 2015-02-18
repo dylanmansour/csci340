695dbe14a1a0acf19053ebc7e525bd17ee1eb667
John Anderson <lampwins@gmail.com>
2015-02-18 14:55:20 -0500

    first commit

---
+++ a1/hw1.c
@@ -0,0 +1,72 @@
+// ----------------------------------------------
+// These are the only libraries that can be 
+// used. Under no circumstances can additional 
+// libraries be included
+#include <stdio.h>	// IO functions
+#include <stdlib.h> 	// atof function
+#include <math.h>	// math functions
+#include "utils.h"	// your own functions
+
+// ----------------------------------------------
+// Define constants
+#define TOTAL_VECS 10	// constant that defines the total number 
+			// of vectors, i.e. magnitude,direction 
+			// pairs, in the vectors text file.
+
+// -----------------------------------
+// Main function 
+// Arguments:	argc = number of arguments suppled by user
+//		argv = array of argument values
+//
+//
+int main( int argc, char** argv ) {
+	
+	// --------------------------------
+	// Steps to be performed
+	//	1. Define variables (including arrays)
+	//	2. Using read() function in utils.h, read vectors defined in text file 
+	//		- the location of the text file (i.e. vectors.txt) must be defined in argv[1]
+	//  	3. Loop through array of vector structs and print to console:
+	//		- vector magnitude (using only 2 decimal places)
+	//		- vector direction (in radians using only 2 decimal places)
+	//		- vector direction (in degrees using only 2 decimal places)
+	//		- Using compute_x() function in utils.h, x component value (using only 2 decimal places)
+	//		- Using compute_y() function in utils.h, y component value (using only 2 decimal places)
+	//	    in the following format
+	//		r = <value>, theta = <value> degrees, <value> radians, x_comp = <value>, y_comp = <value>
+	//	    For example:
+	//		r = 10.00, theta = 180.00 degrees, theta = 3.14 radians, x_comp = -10.00, y_comp = 0.00 
+	//
+	
+	//create v_struct array with given space
+	v_struct p_vec_array[TOTAL_VECS];		
+
+	//read in the file of vectors and fill the array
+	int count = read(argv[1], p_vec_array);	
+	
+	//check that number read in is as expected
+	if(count != TOTAL_VECS){
+		printf("Error: the total number of vectors read was %d, should have been %d\n", count, TOTAL_VECS);
+		exit(1);
+	}
+	
+	//loop through the array and print out results
+	for(int i = 0; i < TOTAL_VECS; i++){
+		
+		//print r and theta
+		printf("r = %.2f, theta = %.2f degrees, ", p_vec_array[i].r, p_vec_array[i].theta);
+		
+		//convert from degrees to radians
+		degrees_to_radians(&p_vec_array[i]);
+		
+		//print theta (radians)
+		printf("theta = %.2f radians, ", p_vec_array[i].theta);
+		
+		//print x and y
+		printf("x = %.2f, y = %.2f\n", x_component(&p_vec_array[i]), y_component(&p_vec_array[i]));
+	}
+	
+	//done!
+	return 0;
+
+} // end main() function
---
+++ a1/utils.c
@@ -0,0 +1,120 @@
+// -----------------------------------
+// CSCI 340 - Operating Systems I 
+// Spring 2015 (Tony Leclerc, Brent Munsell)
+// utils.c implementation file
+// Homework Assignment 1
+//
+// -----------------------------------
+
+// ----------------------------------------------
+// These are the only libraries that can be 
+// used. Under no circumstances can additional 
+// libraries be included
+#include <stdio.h>	// IO functions
+#include <stdlib.h> 	// atof function
+#include <math.h>	// math functions
+#include "utils.h"	// your own functions
+
+// Now you put your implementation of the function prototypes here...
+
+// -------------------------
+// function read
+// returns: int - number of v_structs read in from givin file
+// params: file_name - char*, name of the file containing v_structs
+//         p_vec_array - v_struct*, of v_structs that is to be populated
+//
+int read(char* file_name, v_struct* p_vec_array){
+	
+	//open file
+	FILE * f = fopen(file_name, "r");
+	
+	//create space to store a line of the file
+	char vec[100];
+	
+	//counter for number of vectors read in
+	int counter = 0;
+
+	//error reading the file
+	if(f == NULL){
+		printf("Error reading in file\n");
+		exit(1);
+	}	
+
+	//loop through the file and read in each line
+	while(fgets(vec, 100, f) != NULL){
+		
+		int pos = 0;
+		double r, theta;
+		
+		//find position of the comma		
+		while(vec[pos] != ',' && vec[pos] != '\0')
+			pos++;
+		
+		if(vec[pos] == ','){
+			
+			//set the common position to the null value
+			vec[pos] = '\0';
+			
+			//read in the just part of the string until the null value and convert to double
+			r = atof(vec);
+			
+			//read in second half of the line and convert to double
+			theta = atof(&vec[pos+1]);			
+			
+			//set values of the v_struct
+			p_vec_array[counter].r = r;
+			p_vec_array[counter].theta = theta;
+			
+			//increment counter
+			counter++;
+		}	
+	
+	}
+
+	//return the number of vectors read in
+	return counter;
+}
+
+
+// -----------------------------
+// function x_component
+// returns: double - represents x component value of vector
+// params: p_vector = pointer to v_struct
+// 
+double x_component(v_struct* p_vector){
+	
+	//compute and return x value
+	return p_vector->r * cos(p_vector->theta);
+}
+
+// -----------------------------
+// function y_component
+// returns: double - represents y component value of vector
+// params: p_vector = pointer to v_struct
+// 
+double y_component(v_struct* p_vector){
+	
+	//compute and return y value
+	return p_vector->r * sin(p_vector->theta);
+}
+
+// -----------------------------
+// function degrees_to_radians
+// returns: void
+// params: p_vector = pointer to v_struct
+// 
+void degrees_to_radians( v_struct* p_vector ){
+	
+	//get the degree value of the v_struct
+	double degrees = p_vector->theta;
+	
+	/*if(degrees > 360){
+		degrees -= 360;
+	}else if(degrees < -360){
+		degrees += 360;
+	}*/
+	
+	//convert and set back to theta
+	p_vector->theta = degrees * PI/180;
+	 
+}
